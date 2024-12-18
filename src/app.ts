bdc71a30fcbcf0dc3ac0a9dcc6fd6c294863c644
alamin-cse44 <alamincse44@gmail.com>
2024-12-19 00:21:36 +0600

    Feat: catchAsync & globalError handler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ const app: Application = express();
 import cors from 'cors';
 import router from './app/routes';
 import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 // parser
 app.use(express.json());
@@ -14,6 +15,9 @@ app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Blog Project!');
 });
+
+// global error handler
+app.use(globalErrorHandler);
 
 // not found
 app.use(notFound);
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,24 +2,18 @@ import { Request, Response } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const result = await UserServices.registerUserIntoDB(req.body);
+const registerUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserServices.registerUserIntoDB(req.body);
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      success: true,
-      message: 'User registered successfully',
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: error.message,
-      success: false,
-    });
-  }
-};
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'User registered successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   registerUser,
