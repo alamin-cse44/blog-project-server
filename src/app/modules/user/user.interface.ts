94420e7a6dd060fcb727c4d2a01986511441156a
alamin-cse44 <alamincse44@gmail.com>
2024-12-19 20:54:53 +0600

    Add: statics

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,15 +1,22 @@
-
+import { Model } from 'mongoose';
 
 export interface IUser {
-    name: string;
-    email: string;
-    password: string;
-    role: "admin" | "user";
-    isBlocked: boolean;
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'user';
+  isBlocked: boolean;
 }
 
-
 export type TLoginUser = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
+};
+
+export interface UserModel extends Model<IUser> {
+  findUserByEmail(email: string): Promise<IUser>;
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,24 +1,43 @@
-import { IUser, TLoginUser } from "./user.interface"
-import { User } from "./user.model"
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/AppError';
+import { IUser, TLoginUser } from './user.interface';
+import { User } from './user.model';
 
+const registerUserIntoDB = async (payload: IUser) => {
+  const result = await User.create(payload);
 
-const registerUserIntoDB = async(payload: IUser) => {
+  return result;
+};
 
-    const result = await User.create(payload)
+const loginUserIntoDB = async (payload: TLoginUser) => {
+  // check if user is exist with the email address
+  const user = await User.findUserByEmail(payload?.email);
+  console.log('user: ', user);
 
-    return result;
-}
+  if (!user) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'The User is not found!!!');
+  }
 
+  // check if user is blocked
+  if (user.isBlocked) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'The User is blocked!!!');
+  }
 
-const loginUserIntoDB = async(payload: TLoginUser) => {
-    if(payload?.email && payload?.password){
-        return await User.findOne({ email: payload?.email, password: payload?.password})
-    }
-}
+  // check if password is correct
+  const isPasswordCorrect = await User.isPasswordMatched(
+    payload?.password,
+    user?.password,
+  );
 
+  if (!isPasswordCorrect) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'The password is incorrect!!!');
+  }
+
+  // create token to send to the client 
+  
+};
 
 export const UserServices = {
-    registerUserIntoDB,
-    loginUserIntoDB,
-
-}
+  registerUserIntoDB,
+  loginUserIntoDB,
+};
